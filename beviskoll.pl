90b479fee2ef20e6af12e88af660108fe8f96ea6
Lucasljungberg <lucasljungberg@gmail.com>
2015-10-12 19:51:06 +0200

    Continued on solutions

--- beviskoll.pl
+++ beviskoll.pl
@@ -9,26 +9,25 @@ verify(InputFileName) :-
     see(InputFileName), read(Prems), read(Goal), read(Proof),
     seen,
     valid_proof(Goal, Proof),
-    iterate(Prems, Proof).
+    iterate(Prems, Proof, Proof).
 
 valid_proof(Goal, Proof) :-
     check_goal(Goal, Proof)
     .
 
-iterate(_, []) :- !.
-iterate(Prems, [Head | Tail]) :-
+iterate(_, [], _) :- !.
+iterate(Prems, [Head | Tail], Proof) :-
     check_proof(Prems, Head, Proof),
-    iterate(Prems, Tail).
+    iterate(Prems, Tail, Proof).
     
 
-check_lines(Action, Line1, Line2, Rule, Proof) :-
+check_lines(_, Line1, Line2, _, Proof, Action1, Action2) :-
     Row1 is Line1 - 1,
     Row2 is Line2 - 1,
     nth0(Row1, Proof, Proof1),  %% Loads the proofrows from the specified lines
     nth0(Row2, Proof, Proof2),
     nth0(1, Proof1, Action1),   %% Loads the action from each line
-    nth0(1, Proof2, Action2),
-    member(Action1, Action2).
+    nth0(1, Proof2, Action2).
     
 
 %% Checks if the last line of proof matches with goal 
@@ -40,18 +39,24 @@ check_goal(Goal, Proof) :-
 
 check_proof(_, [], _) :- !.
 
-check_proof(Prems, [Nr, Action, premise], Proof) :-
+check_proof(Prems, [_, Action, premise], _) :-
     member(Action, Prems), !.
 
+check_proof(_, [_, and(X,Y), Rule], Proof) :-
+    check_rule(imp(X,Y), Rule, Proof), !. 
+
 %% Finds match for imp rule as action
-check_proof(_, [Nr, imp(X,Y), Rule], Proof) :-
+check_proof(_, [_, imp(X,Y), Rule], Proof) :-
     check_rule(imp(X,Y), Rule, Proof), !. 
     
-check_proof(_, [Nr, X, Rule], Proof) :-
+check_proof(_, [_, X, Rule], Proof) :-
     check_rule(X, Rule, Proof), !.
     
 check_rule(Action, impel(X,Y), Proof) :-
-    check_lines(Action, X, Y, impel(X,Y), Proof), !. 
+    check_lines(Action, X, Y, impel(X,Y), Proof, Action1, Action2),
+    imp(Action1, Action) = Action2, !. 
+
+
     
     
     
